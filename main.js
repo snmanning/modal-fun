00b1194cba5d07267fbf7d20063aa4545917b92c
Sarah Manning <sarahnmanning@gmail.com>
2018-06-28 19:53:45 -0400

    made the modal show and hide as well as have a dynamic image element inside the modal.

---
+++ main.js
@@ -0,0 +1,52 @@
+const modal = {
+
+    isHidden: true,
+    selectedImage: '',
+
+    init() {
+        this.cacheDom();
+        this.bindEventListeners();
+        this.render();
+    },
+
+    cacheDom() {
+        this.modalContainer = document.querySelector('.modal-container');
+        this.closeBtn = document.querySelector('.close');
+        this.modalImage = document.querySelector('.modal-image');
+        this.thumbnails = document.querySelectorAll('.thumbnail');
+    },
+
+    bindEventListeners() {
+        this.closeBtn.addEventListener('click', this.hideModal.bind(this));
+        this.thumbnails.forEach((thumbnail) => {
+            thumbnail.addEventListener('click', this.showModal.bind(this));
+        });
+    },
+
+    showModal(evt) {
+        this.isHidden = false;
+        this.selectedImage = evt.target.src;
+        this.render();
+    },
+
+    hideModal() {
+        this.isHidden = true;
+        this.selectedImage = '';
+        this.render();
+},
+
+    render() {
+        if(this.isHidden) {
+            this.modalContainer.classList.add('hidden');
+        } else {
+            this.modalContainer.classList.remove('hidden');
+            this.modalImage.src = this.selectedImage;
+        }
+    },
+
+
+};
+
+modal.init();
+
+window.modal = modal;
